de86fc7faae2c3d2e0f3af9a63346219931098bc
kelvenwyllames <kelvenwyllames@gmail.com>
2024-03-23 20:02:52 -0300

    Atualizando estilos CSS e componentes Angular
    - Atualizou estilos CSS para melhorar a aparência da página
    - Adicionando inputs de entrada de dados para os componentes Angular
    - Corrigindo links das redes sociais no HTML

--- src/app/components/social-media/social-media.component.ts
+++ src/app/components/social-media/social-media.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-social-media',
   standalone: true,
   imports: [],
   templateUrl: './social-media.component.html',
-  styleUrl: './social-media.component.css'
+  styleUrl: './social-media.component.css',
 })
 export class SocialMediaComponent {
+  @Input()
+  linkUrl: string = '';
 
+  @Input()
+  imgSocial: string = '';
+
+  @Input()
+  altImg: string = '';
+
+  @Input()
+  userSocial: string = '';
 }
